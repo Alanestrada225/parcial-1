1d569620d7225ed030c6cfb9088c1249db060d97
Alanestrada225 <60673860+Alanestrada225@users.noreply.github.com>
2020-02-06 16:52:05 -0600

    Add files via upload

---
+++ menor.js
@@ -0,0 +1,16 @@
+var num1=Math.floor(math.random()*20)+1;
+var num2=Math.floor(math.random()*20)+1;
+var num3=Math.floor(math.random()*20)+1;
+
+if (num1<num2)
+   if (num1<num3)
+       console.log(num1);
+      else
+       console.log(num3);
+else 
+   if (num2<num3)
+    console.lon(num2);
+   else
+   console.lon(num3);
+
+   console.log (num1+ "    " + num2 +"    " + num3);
